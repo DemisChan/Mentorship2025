8acb47d8fb28895c046849b9260b0dd5d5d0bc12
Demis Lavrentidis <demis.lavrentidis@atombank.co.uk>
2025-05-02 11:58:11 +0100

    Add substring and window utility functions
    -Introduced `lengthOfLongestSubstring` to compute the length of the longest substring without repeating characters.
    -Added `maxValue` to find the maximum values in k-sized sliding windows of an array. Expanded the main function with examples to test these utilities.

--- All/src/main/kotlin/week2/src/main/kotlin/Tasks.kt
+++ All/src/main/kotlin/week2/src/main/kotlin/Tasks.kt
@@ -1,5 +1,6 @@
 package org.mentorship2025.week2.src.main.kotlin
 
+import org.mentorship2025.week1.src.collectionsHighOrderFunctions.longestString
 import kotlin.math.abs
 
 /* Find the Missing Number in an Array
@@ -40,6 +41,49 @@ fun isBalanced(input: String): Boolean {
     return br1 == br2 && br2 == br3
 }
 
+/*
+Find the Longest Substring Without Repeating Characters
+Given a string, return the length of the longest substring without repeating characters.
+Example: "abcabcbb" → 3
+*/
+
+fun lengthOfLongestSubstring(s: String): Int {
+    var longest = 0
+    var start = 0
+    if (s.isEmpty()) return 0
+
+    val lastSeen = HashMap<Char, Int>()
+    for (end in s.indices) {
+        val currentChar = s[end]
+        if (lastSeen.contains(currentChar) && lastSeen[currentChar]!! >= start) {
+            start = lastSeen[currentChar]!! + 1
+        } else {
+            longest = maxOf(longest, end - start + 1)
+        }
+        lastSeen[currentChar] = end
+    }
+    return longest
+}
+
+/*
+Maximum Value in K-sized Window
+Given an array and an integer k, return the maximum value in every k-sized window.
+ */
+
+fun maxValue(arr: IntArray, k: Int): List<Int> {
+    var temp = k
+    val result = IntArray(arr.size - k + 1)
+    for (i in 0..arr.size - k) {
+        if (temp <= arr.size) {
+            val x = arr.slice(i until temp)
+            result[i] = x.max()
+            temp += 1
+        }
+
+    }
+    return result.toList()
+}
+
 
 fun main() {
     println(findMissingNumber(intArrayOf(3, 0, 1))) // Expected: 2
@@ -50,5 +94,14 @@ fun main() {
     println(isBalanced("{[}]"))
     println(isBalanced("a(b)c[d]e{f}"))
 
+
+    println(lengthOfLongestSubstring("abcabcbb"))
+    println(lengthOfLongestSubstring("bbbbb"))
+    println(lengthOfLongestSubstring("abcdefggghjjkiolslkjhbfdljdhvbasdf"))
+    println(lengthOfLongestSubstring(""))
+
+
+    println(maxValue(intArrayOf(1, 4, 2, 10, 2, 3, 1, 0, 20), 4))
+    println(maxValue(intArrayOf(5, 4, 3, 2, 1), 3))
 
 }
